5b918be8b2a176ab72e7b9b081105ad1540a0b12
Duc Harper <djharper757@gmail.com>
2026-07-05 23:48:50 -0400

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int first = 0;
+        int second = 0;
+
+        for (int stone : stones) {
+            pq.add(stone);
+        }
+
+        while (pq.size() > 1) {
+            first = pq.poll();
+            second = pq.poll();
+
+            if (first == second) {
+                continue;
+            } else if (first > second) {
+                pq.add(first - second);
+            }
+        }
+
+        if (pq.size() == 0) {
+            return 0;
+        } else {
+            return pq.poll();
+        }
+    }
+}
